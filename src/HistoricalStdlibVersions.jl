a590930e616d7aebd69204f7d8d49490fcdb2f9c
Elliot Saba <staticfloat@gmail.com>
2024-06-02 08:31:39 -0700

    Add out of date warning (#22)
    * Add autodetection for out-of-date HSV mappings
    
    This will just alert us when a new Julia build is beyond the edge of
    what is recorded in HSV.
    
    * Release v1.2.4

--- src/HistoricalStdlibVersions.jl
+++ src/HistoricalStdlibVersions.jl
@@ -14,4 +14,13 @@ module HistoricalStdlibVersions
 
 include("version_map.jl")
 
+let
+    max_hsg_version = maximum(first.(STDLIBS_BY_VERSION))
+    # Throw a warning at compile-time if VERSION looks like it's a major or minor version ahead
+    # of the latest version captured within `version_map.jl`.  This assumes that we bump at least
+    # one stdlib every minor release, which so far appears to be a safe bet.
+    if VersionNumber(max_hsg_version.major, max_hsg_version.minor) < VersionNumber(VERSION.major, VERSION.minor)
+        @warn("HistoricalStdlibVersions seems to be out of date; please open an issue at https://github.com/JuliaPackaging/HistoricalStdlibVersions.jl/issues")
+    end
+end
 end # module HistoricalStdlibVersions
