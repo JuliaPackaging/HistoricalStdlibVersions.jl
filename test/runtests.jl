795efbce186f95164d87d02188a0d0d6404d7132
Elliot Saba <staticfloat@gmail.com>
2024-06-02 12:24:55 -0700

    Rewrite for dependency tracking
    This significantly reworks the datastructures used to encode the
    historical stdlib data, so that we can include dependencies and weak
    dependencies for all stdlib versions.  This will be used by Pkg to
    properly resolve transitive dependencies of stdlibs.
    
    This also changes `HistoricalStdlibVersions` to register the data
    structures with `Pkg` on load.  This simplifies things greatly, and also
    allows us to maintain a single location that knows how to deal with
    multiple different `Pkg` versions, as only Julia v1.12+ will know how to
    use the newer datastructures here.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,4 @@ import HistoricalStdlibVersions
 import Pkg
 using Test
 
-append!(empty!(Pkg.Types.STDLIBS_BY_VERSION), HistoricalStdlibVersions.STDLIBS_BY_VERSION)
-
 @test !isempty(Pkg.Types.STDLIBS_BY_VERSION)
