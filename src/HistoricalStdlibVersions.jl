597b3e6b5ed5992b0d4c81f6a966922988023e89
Elliot Saba <staticfloat@gmail.com>
2025-01-20 05:23:45 +0000

    Fix running HSG on newer Julia versions
    Newer Julia versions don't have `StdlibInfo` defined in `Pkg.Types`, so
    we need to work around that in a few places to make sure we can `eval()`
    code from all Julia versions.

--- src/HistoricalStdlibVersions.jl
+++ src/HistoricalStdlibVersions.jl
@@ -5,24 +5,7 @@ Loads historical stdlib version information into Pkg to allow Pkg to resolve std
 """
 module HistoricalStdlibVersions
 using Pkg
-import Base: UUID
-
-# Use the `Pkg` `StdlibInfo` type if it exists, otherwise just re-define it
-if !isdefined(Pkg.Types, :StdlibInfo)
-struct StdlibInfo
-    name::String
-    uuid::UUID
-
-    # This can be `nothing` if it's an unregistered stdlib
-    version::Union{Nothing,VersionNumber}
-
-    deps::Vector{UUID}
-    weakdeps::Vector{UUID}
-end
-else
-import Pkg.Types: StdlibInfo
-end
-
+include("StdlibInfo.jl")
 include("version_map.jl")
 
 let
